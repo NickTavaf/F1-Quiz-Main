39dedc1891a8332b0baf9fe92fedf868f25c3512
NickTavaf <tavan388@newschool.edu>
2025-04-22 09:24:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         setTimeout(() => {
           window.location.href = nextPage;
-        }, 2000);
+        }, 1000);
       } else {
         if (feedback) {
           feedback.textContent = "Try again!";
